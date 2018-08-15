a63eb6a2f73d8f4a47d4b80d9373b7fb3507ddc9
tiger_miao <tiger.miao.ql@gmail.com>
2018-08-15 22:11:30 +0800

    增加了Question模型及其相应的单元测试

--- onlineexam/tests.py
+++ onlineexam/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from django.http import HttpRequest
 
 from .views import home_page
+from .models import Question
 
 class HomePageTest(TestCase):
 
@@ -11,3 +12,21 @@ class HomePageTest(TestCase):
 
         self.assertTemplateUsed(response, 'onlineexam/home.html')
 
+class QuestionModelTest(TestCase):
+
+    def test_save_and_retrieving_questions(self):
+        first_question = Question()
+        first_question.text = 'The first question'
+        first_question.save()
+
+        second_question = Question()
+        second_question.text = 'The second question'
+        second_question.save()
+
+        saved_questions = Question.objects.all()
+        self.assertEqual(saved_questions.count(), 2)
+
+        first_saved_question = saved_questions[0]
+        second_saved_question = saved_questions[1]
+        self.assertEqual(first_saved_question.text, 'The first question')
+        self.assertEqual(second_saved_question.text, 'The second question')
